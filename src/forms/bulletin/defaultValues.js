a45bca4149dc30ac0041bf00ca749e6bd0711f50
TatySCV <tatiana.campos03@inacapmail.cl>
2026-07-07 11:22:21 -0400

    feat: add defaultValues and validation files for bulletin form

---
+++ src/forms/bulletin/defaultValues.js
@@ -0,0 +1,14 @@
+const defaultValues = {
+  firstName: "",
+  lastName: "",
+  document: "",
+  sex: "",
+  birthDate: "",
+  nationality: "",
+  photo: null,
+  expulsions: [],
+  antecedentes: [],
+  observations: "",
+};
+
+export default defaultValues;
